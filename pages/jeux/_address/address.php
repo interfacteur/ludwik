c095540fceec3a1d8eaeceeadf41af913888826b
Interfacteur <gaetan@interfacteur.net>
2016-02-13 19:49:34 +0100

    address

--- pages/jeux/_address/address.php
+++ pages/jeux/_address/address.php
@@ -2,10 +2,10 @@
 $page = (count($_GET) > 0 && ! empty($_GET["page"])) ? htmlentities($_GET["page"]) : "vide";
 $pages = array(
 	"vide" => '',
-	"tailles" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-2-jeu-tailles.html">du jeu des tailles</a> et ',
-	"cache" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-3-cache-cache.html">du jeu de cache-cache</a> et ',
-	"puzzle" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-4-puzzle.html">du puzzle</a> et ',
-	"milieux" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-5-jeu-milieux.html">du jeu des milieux</a> et '
+	"tailles" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-2-jeu-tailles.html">jeu des tailles</a> ; ',
+	"cache" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-3-cache-cache.html">jeu de cache-cache</a> ; ',
+	"puzzle" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-4-puzzle.html">puzzle</a> ; ',
+	"milieux" => '<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-5-jeu-milieux.html">jeu des milieux</a> ; '
 );
 $depots = array(
 	"vide" => '',
@@ -16,9 +16,10 @@ $depots = array(
 )
 ?>
 <address class="address" <?php if (isset($lang)) { echo 'lang="fr"'; } ?>>
-	Notes de présentation <?php echo $pages[$page]; ?>
-	<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-1-presentation.html">de tous les jeux</a> -
+	Notes de présentation : <?php echo $pages[$page]; ?>
+	<a href="http://interfacteur.blogspot.fr/2015/06/jeux-d-oiseaux-1-presentation.html">tous les jeux</a> -<br>
 	<?php if (! isset($modele)) { echo $depots[$page]; } ?>
-	<a href="https://github.com/interfacteur/ludwik">Github</a> -
-	<a href="http://gaetan.langhade.net">Gaëtan Langhade, Interfacteur</a> - pour <a href="http://www.ludwik.fr/">Ludwik</a><br>
+	<a href="https://github.com/interfacteur/ludwik">Github</a> -<br>
+	<a href="http://gaetan.langhade.net">Gaëtan Langhade, Interfacteur</a> -<br>
+	pour <a href="http://www.ludwik.fr/">Ludwik</a><br>
 </address>
